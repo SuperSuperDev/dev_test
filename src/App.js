ccc725de61e2d456df73e8459dbc46120ff85277
SuperSuperStore <steven@supersuperstore.com>
2022-12-02 22:40:24 +0000

    init AnimatedPlanet Component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import Home from './pages/Home';
 import Hero from './components/Hero';
 import style from './App.module.scss';
 import ExoPlanets from './pages/ExoPlanets';
+import AnimatedPlanet from './components/AnimatedPlanet';
 
 const App = () => (
   <div className={style.main}>
     <Hero />
+    <AnimatedPlanet />
     <Home />
     <ExoPlanets />
   </div>
 );
 
 export default App;
-
---
+++ src/components/AnimatedPlanet/index.js
@@ -0,0 +1,5 @@
+const AnimatedPlanet = ({ pl_name }) => {
+  return <h1>Planet Here</h1>;
+};
+
+export default AnimatedPlanet;
