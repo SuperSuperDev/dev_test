71b2809e4a6cf86ce52dd105e3bbc6829b13cc2b
SuperSuperStore <steven@supersuperstore.com>
2022-12-03 15:50:20 +0000

    feat: routes with react-router-dom
    Added routes for home (/) and ExoPlanets (/exoplanets)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Hero from './components/Hero';
 import style from './App.module.scss';
 import ExoPlanets from './pages/ExoPlanets';
 
 const App = () => (
-  <div className={style.main}>
-    <Hero />
-    <Home />
-    <ExoPlanets />
-  </div>
+  <BrowserRouter>
+    <div className={style.main}>
+      <Hero />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exoplanets" element={<ExoPlanets />} />
+      </Routes>
+      <ExoPlanets />
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
